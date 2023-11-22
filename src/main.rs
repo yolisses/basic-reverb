96b97911e3998ef9bd368b108881da8c1ded7884
yolisses <iamurise@gmail.com>
2023-11-22 17:29:28 -0300

    use wav files to test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::basic_reverb::BasicReverb;
 use constants::CHANNELS;
+use hound::{WavReader, WavWriter};
 mod array;
 mod basic_reverb;
 mod constants;
@@ -13,34 +14,47 @@ mod householder_test;
 mod multi_channel_mixed_feedback;
 mod sample_rate;
 
-pub fn main() {
-    let input = [
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-        0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-    ];
+fn main() -> Result<(), hound::Error> {
+    // Input WAV file path
+    let input_path = "guitar.wav";
 
-    let room_size_ms: f64 = 500.;
-    let rt60: f64 = 0.5;
-    let dry: f64 = 0.2;
-    let wet: f64 = 0.8;
+    // Output WAV file path
+    let output_path = "out.wav";
+
+    // Open the input WAV file for reading
+    let mut reader = WavReader::open(input_path).unwrap();
+
+    // Get WAV file metadata
+    let spec = reader.spec();
+
+    // Create a new WAV file for writing
+    let mut writer = WavWriter::create(output_path, spec)?;
+
+    let room_size_ms: f64 = 100.;
+    let rt60: f64 = 10.;
+    let dry: f64 = 0.;
+    let wet: f64 = 1.;
     let mut basic_reverb = BasicReverb::new(room_size_ms, rt60, dry, wet);
 
-    let mut result = vec![];
-    for sample in input {
-        let output = basic_reverb.process([sample; CHANNELS]);
+    // run_debug(&mut basic_reverb);
+
+    // Iterate over samples and multiply each sample by 10
+    for sample in reader.samples::<i16>() {
+        let sample = sample?;
+        let sample = sample as f64 / i16::MAX as f64;
+
+        let output_channels = basic_reverb.process([sample; CHANNELS]);
         let mut sum = 0.;
+
         for c in 0..CHANNELS {
-            sum += output[c];
+            sum += output_channels[c];
         }
-        result.push(sum);
+
+        let processed_sample = (i16::MAX as f64 * sum) as i16;
+
+        // Write the processed sample to the output WAV file
+        writer.write_sample(processed_sample)?;
     }
 
-    for value in result {
-        print!("{:.2e} ", value);
-    }
+    Ok(())
 }
