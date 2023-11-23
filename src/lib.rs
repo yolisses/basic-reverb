c42db7359b1cdfdd27209fa8efcb58589b082ca9
yolisses <iamurise@gmail.com>
2023-11-23 20:34:30 -0300

    use pub mod in basic_reverb

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+pub mod basic_reverb;
+pub mod mono_basic_reverb;
+pub mod stereo_basic_reverb;
+
 mod array;
-mod basic_reverb;
 mod constants;
 mod delay;
 mod diffuser;
 mod mean;
 mod mix_matrix;
-mod mono_basic_reverb;
 mod multi_channel_mixed_feedback;
-mod stereo_basic_reverb;
