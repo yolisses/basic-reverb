08e669b85111f2720bebc541a009d7c81795d8cc
yolisses <iamurise@gmail.com>
2023-11-26 17:50:13 -0300

    fix resized_input

--- src/mono_basic_reverb.rs
+++ src/mono_basic_reverb.rs
@@ -10,17 +10,24 @@ impl MonoBasicReverb {
         rt60: f64,
         dry: f64,
         wet: f64,
-        channels: usize,
+        channels: u32,
         sample_rate: u32,
     ) -> Self {
         Self {
-            basic_reverb: BasicReverb::new(room_size_ms, rt60, dry, wet, channels, sample_rate),
+            basic_reverb: BasicReverb::new(
+                room_size_ms,
+                rt60,
+                dry,
+                wet,
+                channels as usize,
+                sample_rate,
+            ),
         }
     }
 
     pub fn process_sample(&mut self, sample: f64) -> f64 {
         let channels = self.basic_reverb.channels;
-        let resized_input = Vec::with_capacity(channels);
+        let resized_input = vec![sample; channels];
         let output = self.basic_reverb.process(resized_input);
         mean(&output)
     }
