a6e70024f858b12a23facf88cf77dfa9481a087e
yolisses <iamurise@gmail.com>
2024-04-09 14:11:16 -0300

    install criterion

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,20 @@ mod stereo_basic_reverb;
 pub use basic_reverb::BasicReverb;
 pub use mono_basic_reverb::MonoBasicReverb;
 pub use stereo_basic_reverb::StereoBasicReverb;
+
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
