c99bb24b969ad09f19a24242504a3b1bf10528af
yolisses <iamurise@gmail.com>
2024-04-09 14:21:40 -0300

    update dependencies

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,6 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use basic_reverb::fibonacci;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 30", |b| b.iter(|| fibonacci(black_box(30))));
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
